21f6ec67d2db434b99c2fd64d4e4df23840c5516
Sebastián <ssebass2000@gmail.com>
2021-05-30 17:29:49 +0200

    Gallery Img en New Event añadido

--- app/src/main/java/com/sai/eventsports/principales/ActivityNewEvent.java
+++ app/src/main/java/com/sai/eventsports/principales/ActivityNewEvent.java
@@ -2,17 +2,28 @@ package com.sai.eventsports.principales;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputLayout;
 import com.sai.eventsports.CollectData;
@@ -35,6 +46,10 @@ public class ActivityNewEvent extends AppCompatActivity {
     private Spinner spinnerDeporte;
     private TextInputLayout direccion,num,localidad;
     private Context context = this;
+    private static final int REQUEST_PERMISSON_CODE = 100;
+    private static final int REQUEST_IMAGE_GALLERY = 101;
+    private Uri imageUri;
+    private ImageView galleryImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,5 +128,74 @@ public class ActivityNewEvent extends AppCompatActivity {
                 CollectData.saveEvento(e);
             }
         });
+
+        galleryImage = (ImageView) findViewById(R.id.escogerImagen);
+
+        Glide.with(this)
+                .load("android.resource://" + getPackageName() + "/" + R.drawable.user_profile)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(300))
+                .circleCrop()
+                .into(galleryImage);
+
+        galleryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ActivityCompat.checkSelfPermission(ActivityNewEvent.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        openGallery();
+                    } else {
+                        ActivityCompat.requestPermissions(ActivityNewEvent.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSON_CODE);
+                    }
+                }
+
+            }
+        });
+        Glide.with(getApplicationContext())
+                .load(Uri.parse("https://firebasestorage.googleapis.com/v0/b/stellar-operand-305716.appspot.com/o/FotosPerfil%2F" + ActivityLogIn.USERUID + ".jpg?alt=media&token=7437589e-16cb-4f09-8c58-3b4f1e6187be"))
+                .placeholder(R.drawable.ic_profile)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(300))
+                .circleCrop()
+                .into(galleryImage);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_IMAGE_GALLERY) {
+            imageUri = data.getData();
+
+            Glide.with(this)
+                    .load(imageUri)
+                    .centerCrop()
+                    .transition(DrawableTransitionOptions.withCrossFade(300))
+                    .circleCrop()
+                    .into(galleryImage);
+            //CollectData.saveImg(imageUri);
+        } else {
+            Log.i("RAG", "Result: " + resultCode);
+            Toast.makeText(this, "You did not choose any photo", Toast.LENGTH_SHORT).show();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSON_CODE) {
+            if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                Toast.makeText(this, "You need to enable permissions", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
     }
 }
