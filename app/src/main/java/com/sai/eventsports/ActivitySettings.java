671a0f71e85aa2b3083229aaf619e37079a73a43
Alvaro230 <surfiera@gmail.com>
2021-06-06 21:04:14 +0200

    Cambiar Datos

--- app/src/main/java/com/sai/eventsports/ActivitySettings.java
+++ app/src/main/java/com/sai/eventsports/ActivitySettings.java
@@ -13,7 +13,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.sai.eventsports.splash_login_register.ActivityChangePhone;
 import com.sai.eventsports.splash_login_register.ActivityLogIn;
 
 public class ActivitySettings extends AppCompatActivity {
